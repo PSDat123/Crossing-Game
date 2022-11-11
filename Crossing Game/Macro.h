d385b3d90664e3c7a2b8a4fa33486b18d4e18dc4
Faith <faithdanghuy@gmail.com>
2022-11-11 12:37:50 +0700

    Enum class updates

--- Crossing Game/Macro.h
+++ Crossing Game/Macro.h
@@ -3,10 +3,25 @@
 #define DOWN_ARROW 80
 #define ENTER_KEY 13
 
-
 enum class DIRECTION {
 	UP,
 	DOWN,
 	LEFT,
 	RIGHT
+};
+
+enum class GameState {
+	MENUING,
+	RUNNING,
+	PAUSED,
+	EXITED
+};
+
+enum class OPTIONS {
+	CONTINUE,
+	NEW_GAME,
+	LOAD_GAME,
+	SETTINGS,
+	CREDIT,
+	EXIT
 };
