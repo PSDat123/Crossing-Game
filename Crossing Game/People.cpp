df850225dc603ee991227ffba2969f2c162b0d25
Faith <faithdanghuy@gmail.com>
2022-11-11 12:18:58 +0700

    Update People class

--- Crossing Game/People.cpp
+++ Crossing Game/People.cpp
@@ -1,1 +1,29 @@
 #include "People.h"
+
+People::People(int cur_x, int cur_y) {
+	this->x = cur_x;
+	this->y = cur_y;
+	this->state = false;
+}
+
+bool People::checkFrame(int a, int b) {
+
+}
+
+bool People::isFinish() {
+	return (x == 0);
+}
+
+int People::getX() {
+	return this->x;
+}
+
+int People::getY() {
+	return this->y;
+}
+
+People::~People() {}
+
+bool People::isDead() {
+	return state;
+}
--- Crossing Game/People.h
+++ Crossing Game/People.h
@@ -1,16 +1,25 @@
 #pragma once
+#include "Vehicle.h"
 
 class People {
 public:
-	People();
+	People(int, int);
+	~People();
+
+	int getX();
+	int getY();
+
 	void Up();
 	void Left();
 	void Right();
 	void Down();
-	//bool isImpact();
+
+	bool isImpact(Vehicle*);
+	bool isImpact();
 	bool isFinish();
 	bool isDead();
 private:
 	int x, y;
 	bool state; // dead/alive
+	bool checkFrame(int,int);
 };
